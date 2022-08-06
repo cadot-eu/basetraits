892a5130090be3af9a4ca211917b417c2c769d21
Michael <m@cadot.eu>
2022-08-06 14:24:22 +0200

    graphisme par xls

---
+++ OrdreTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity\base;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait OrdreTrait
+{
+    #[ORM\Column(type: 'integer', nullable: true)]
+    /**
+     * tpl:no_form
+     */
+    private $ordre;
+    public function getOrdre(): ?int
+    {
+        return $this->ordre;
+    }
+
+    public function setOrdre(?int $ordre): self
+    {
+        $this->ordre = $ordre;
+
+        return $this;
+    }
+}
