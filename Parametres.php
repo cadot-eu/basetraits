288abd24dc078269ba34307972028e93b8ff9bf1
Michael <m@cadot.eu>
2022-12-06 11:37:01 +0100

    en attente nouvel editeur

--- Parametres.php
+++ Parametres.php
@@ -16,138 +16,145 @@ use App\Service\base\ToolsHelper;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 
 #[ORM\Entity(repositoryClass: ParametresRepository::class)]
-#[ORM\HasLifecycleCallbacks()]
+#[ORM\HasLifecycleCallbacks]
 #[UniqueEntity('nom')]
 class Parametres
 {
-    use TimeTrait;
+	use TimeTrait;
 
-    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: Types::INTEGER)]
-    /**
-     * TPL:no_action_add
-     * TPL:no_access_deleted
-     * TPL:no_created
-     * TPL:no_updated
-     * TPL:no_index
-     * ORDRE:{"nom":"ASC"}
-     * SEARCH:['id','nom','valeur']
-     */
-    private $id;
+	/**
+	 * TPL:no_action_add
+	 * TPL:no_access_deleted
+	 * TPL:no_created
+	 * TPL:no_updated
+	 * TPL:no_index
+	 * ORDRE:{"nom":"ASC"}
+	 * SEARCH:['id','nom','valeur']
+	 */
+	#[ORM\Id, ORM\GeneratedValue, ORM\Column(type: Types::INTEGER)]
+	private $id;
 
-    #[ORM\Column(type: Types::STRING, length: 255, unique: true)]
-    /**
-     * attr:{"data-controller" : "base--ckeditor"}
-     * attr:{"data-base--ckeditor-toolbar-value": "§$AtypeOption[\"data\"]->getTypenom()§"}
-     */
-    private $nom;
+	/**
+	 * attr:{"data-controller" : "base--ckeditor"}
+	 * attr:{"data-base--ckeditor-toolbar-value": "§$AtypeOption[\"data\"]->getTypenom()§"}
+	 */
+	#[ORM\Column(type: Types::STRING, length: 255, unique: true)]
+	private $nom;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    /**
-     * attr:{"data-controller" : "base--ckeditor"}
-     * attr:{"data-base--ckeditor-toolbar-value": "§$AtypeOption[\"data\"]->getTypevaleur()§"}
-     */
-    private $valeur;
+	/**
+	 * attr:{"data-controller" : "base--ckeditor"}
+	 * attr:{"data-base--ckeditor-toolbar-value": "§$AtypeOption[\"data\"]->getTypevaleur()§"}
+	 */
+	#[ORM\Column(type: Types::TEXT, nullable: true)]
+	private $valeur;
 
-    #[ORM\Column(type: 'string', length: 255, nullable: true)]
-    /**
-     * readonlyroot
-     * TPL:no_index
-     */
+	/**
+	 * readonlyroot
+	 * TPL:no_index
+	 */
 
-    private $aide;
+	#[ORM\Column(type: 'string', length: 255, nullable: true)]
+	private $aide;
 
-    #[ORM\Column(type: 'string', length: 255, nullable: true)]
-    /**
-     * hiddenroot
-     * TPL:no_index
-     */
-    private $typenom;
+	/**
+	 * hiddenroot
+	 * TPL:no_index
+	 */
+	#[ORM\Column(type: 'string', length: 255, nullable: true)]
+	private $typenom;
 
-    #[ORM\Column(type: 'string', length: 255, nullable: true)]
-    /**
-     * hiddenroot
-     * TPL:no_index
-     */
-    private $typevaleur;
+	/**
+	 * hiddenroot
+	 * TPL:no_index
+	 */
+	#[ORM\Column(type: 'string', length: 255, nullable: true)]
+	private $typevaleur;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
+	public function getNom(): ?string
+	{
+		return $this->nom;
+	}
 
-    public function setNom(string $nom): self
-    {
-        $this->nom = AllExtension::ckclean($nom);
-        return $this;
-    }
+	public function setNom(string $nom): self
+	{
+		$this->nom = AllExtension::ckclean($nom);
+		return $this;
+	}
 
-    public function getValeur(): ?string
-    {
-        return $this->valeur;
-    }
+	public function getValeur(): ?string
+	{
+		return $this->valeur;
+	}
 
-    public function setValeur(?string $valeur): self
-    {
-        if (substr($valeur, 0, strlen('<p>') == '<p>') and substr($valeur, 0, -strlen('</p>') == '</p>')) {
-            $this->valeur = substr(substr($valeur, strlen('<p>')), 0, -strlen('</p>'));
-        } else {
-            $this->valeur = $valeur;
-        }
+	public function setValeur(?string $valeur): self
+	{
+		if (
+			substr($valeur, 0, strlen('<p>') == '<p>') and
+			substr($valeur, 0, -strlen('</p>') == '</p>')
+		) {
+			$this->valeur = substr(
+				substr($valeur, strlen('<p>')),
+				0,
+				-strlen('</p>')
+			);
+		} else {
+			$this->valeur = $valeur;
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getAide(): ?string
-    {
-        return $this->aide;
-    }
+	public function getAide(): ?string
+	{
+		return $this->aide;
+	}
 
-    public function setAide(?string $aide): self
-    {
-        $this->aide = $aide;
+	public function setAide(?string $aide): self
+	{
+		$this->aide = $aide;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getTypenom(): ?string
-    {
-        return $this->typenom;
-    }
+	public function getTypenom(): ?string
+	{
+		return $this->typenom;
+	}
 
-    public function setTypenom(?string $typenom): self
-    {
-        $this->typenom = $typenom;
+	public function setTypenom(?string $typenom): self
+	{
+		$this->typenom = $typenom;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getTypevaleur(): ?string
-    {
-        return $this->typevaleur;
-    }
+	public function getTypevaleur(): ?string
+	{
+		return $this->typevaleur;
+	}
 
-    public function setTypevaleur(?string $typevaleur): self
-    {
-        $this->typevaleur = $typevaleur;
+	public function setTypevaleur(?string $typevaleur): self
+	{
+		$this->typevaleur = $typevaleur;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    #[ORM\Column(type: Types::STRING, length: 255)]
-    #[Gedmo\Slug(fields: ["nom"], unique: true)]
-    /** 
-     * hiddenroot
-     * OPT:{"required":false} 
-     * OPT:{"label":"lien"} 
-     */
-    private $slug;
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
+	/**
+	 * hiddenroot
+	 * OPT:{"required":false}
+	 * OPT:{"label":"lien"}
+	 */
+	#[ORM\Column(type: Types::STRING, length: 255)]
+	#[Gedmo\Slug(fields: ['nom'], unique: true)]
+	private $slug;
+	public function getSlug(): ?string
+	{
+		return $this->slug;
+	}
 }
