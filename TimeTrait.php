04cdf1e15fdb5af2fb03aba631923514da5dc7a9
michael <m@cadot.eu>
2023-05-18 12:18:10 +0200

    refactor(TimeTrait.php): réorganiser les annotations ORM et Gedmo pour améliorer la lisibilité du code refactor(TimeTrait.php): supprimer la méthode updatedTimestamps() et déplacer la logique dans les setters de createdAt et updatedAt feat(TimeTrait.php): ajouter une propriété privée nullable de type DateTime pour createdAt, updatedAt et deletedAt

--- TimeTrait.php
+++ TimeTrait.php
@@ -9,68 +9,58 @@ use Doctrine\ORM\Mapping as ORM;
 use ORM\HasLifecycleCallbacks;
 use DateTime;
 
+/**
+ * @ORM\HasLifecycleCallbacks
+ */
 trait TimeTrait
 {
-    /**
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    #[Gedmo\Timestampable(on: 'create')]
+     /**
      * opt:{"label":"Créé le"}
      * TPL:no_form
      */
+    private ?DateTime $createdAt = null;
+
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
-    private $createdAt;
-    /**
+    #[Gedmo\Timestampable(on: 'update')]
+     /**
      * opt:{"label":"Mis à jour le"}
      * opt:{"widget":"single_text"}
      * attr:{"data-controller":"base--resetinput"}
      */
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
-    private $updatedAt;
-    public function getCreatedAt(): ?DateTime
-    {
-        return $this->createdAt;
-    }
-    public function setCreatedAt(?DateTime $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-        return $this->updatedTimestamps();
-    }
-    public function getUpdatedAt(): ?DateTime
-    {
-        return $this->updatedAt;
-    }
+    private ?DateTime $updatedAt = null;
     public function setUpdatedAt(?DateTime $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
-        return $this->updatedTimestamps();
+        return $this;
     }
+
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     /**
      * TPL:no_form
      * opt:{"label":"Supprimé le"}
      */
-    private $deletedAt;
+    private ?DateTime $deletedAt = null;
 
-    public function getDeletedAt(): ?\DateTime
+    public function getCreatedAt(): ?DateTime
     {
-        return $this->deletedAt;
+        return $this->createdAt;
     }
 
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+    public function setCreatedAt(?DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
     public function setDeletedAt(?DateTime $deletedAt): self
     {
         $this->deletedAt = $deletedAt;
 
-        return $this;
-    }
-    public function updatedTimestamps()
-    {
-
-        $DATENow = new DateTime('now');
-        if ($this->getUpdatedAt() === null) {
-            $this->setUpdatedAt($DATENow);
-        }
-
-        if ($this->getCreatedAt() === null) {
-            $this->setCreatedAt($DATENow);
-        }
         return $this;
     }
 }
